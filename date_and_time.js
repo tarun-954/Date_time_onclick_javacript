77dcb415ac5c4482a43e86585400b04e714e40b6
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2022-12-18 10:33:57 +0530

    Add files via upload

---
+++ date_and_time.js
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <h1>My first javaScript</h1><br>
+        <button type="button" onclick="document.getElementById('Demo').innerHTML=Date()">Click me</button>
+        <p id="Demo"></p>
+        
+    </body>
+</html>
